65d1721de17f35b7d9d613f73c7c87d11b424558
lg550055 <lg550055@hotmail.com>
2022-08-24 13:47:38 -0500

    Replaces calendar by faq

--- nav.js
+++ nav.js
@@ -2,7 +2,7 @@ document.write(`
   <nav>
     <div><a href="./index.html"><img src="img/cflogo.png" alt="logo" /></a></div>
     <a href="./courses.html">Courses</a>
-    <a href="./">Calendar</a>
+    <a href="./faq.html">FAQs</a>
     <a href="./finance.html">Finance</a>
     <button onclick="(()=>applymodal.style.display='block')()">Get started</button>
   </nav>
