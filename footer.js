98a9314f730e7aa4a4004457c9311a7af0c13abe
lg550055 <lg550055@hotmail.com>
2022-08-24 11:56:35 -0500

    Creates footer module

---
+++ footer.js
@@ -0,0 +1,23 @@
+document.write(`
+  <footer>
+    <nav>
+      <div><a href="./index.html"><img src="img/cflogo.png" alt="logo" /></a></div>
+      <a href="./about.html">About</a>
+      <a href="./faq.html">FAQs</a>
+      <a href="./legal.html">Legal</a>
+      <a href="./courses.html">Courses</a>
+    </nav>
+    <div>
+      <br>
+      <a href="https://www.instagram.com/CodeFusionLLC"><img src="./img/icons/igramblue.svg" alt="instagram"></a>
+      <a href="https://www.facebook.com/codefusionsocial"><img src="./img/icons/fbblue.svg" alt="facebook"></a>
+      <a href="https://www.linkedin.com/company/code-fusion"><img src="./img/icons/linkedinblue.svg" alt="linkedin"></a>
+      <!-- <a href="https://www.youtube.com/watch?v=9WdQvAxcxzA"><img src="./img/icons/ytubeblue.svg" alt="youtube"></a> -->
+      <a href="https://twitter.com/CodeFusionLLC"><img src="./img/icons/twitterblue.svg" alt="twitter"></a>
+      <br>
+      <p>info@code-fusion.com . (830)219-2775</p>
+      <p>Code Fusion is licensed by the Texas Workforce Commission</p>
+      <p>2022 &copy; Code Fusion, LLC - all rights reserved</p>  
+    </div>
+  </footer>
+`);
